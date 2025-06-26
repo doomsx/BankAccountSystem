9163976bce696f512a8b635d9a40c7b809d1d9a5
Dominique <dlmagat08@gmail.com>
2025-06-26 20:51:13 +0800

    spacing

--- Account.java
+++ Account.java
@@ -16,12 +16,10 @@ class Account {
     public int getPIN() { return PIN; }
     public float getBalance() { return balance; }
 
-
     public void setName(String name) { this.name = name; }
     public void setAccountNumber(int accountNumber) { AccountNumber = accountNumber; }
     public void setPIN(int pIN) { PIN = pIN; }
     public void setBalance(float balance) { this.balance = balance; }
-
 
     @Override
     public String toString(){return "Name: " + name + "\nAccount Number: " + AccountNumber + "\nBalance: " + balance;}
